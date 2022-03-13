d2a7fad005ce0697db364ed7bf8e8ee81e53ba8c
DoPanik <963151+DoPaNik@users.noreply.github.com>
2022-03-13 23:35:57 +0100

    :art: Formatting...

--- Scriptable/my-bmw/api.js
+++ Scriptable/my-bmw/api.js
@@ -1,5 +1,5 @@
-const configuration = importModule('config');
-const utils = importModule('utils');
+const configuration = importModule("config");
+const utils = importModule("utils");
 
 const URL = `https://cocoapi.bmwgroup.com/`;
 const VEHICLE_IMAGE_URL = `${URL}eadrax-ics/v3/presentation/vehicles/${configuration.VIN}/images`;
@@ -52,29 +52,29 @@ module.exports.fetchVehicleData = async function () {
 };
 
 async function getAPIToken() {
-  console.log('checking keychain for API token...');
-  if (!Keychain.contains('bmw_access_token')) {
-    console.log('no token found...');
+  console.log("checking keychain for API token...");
+  if (!Keychain.contains("bmw_access_token")) {
+    console.log("no token found...");
     if (!(await requestAPIToken())) return;
     return getAPIToken();
   }
-  console.log('checking if its stil valid...');
-  const token = JSON.parse(Keychain.get('bmw_access_token'));
+  console.log("checking if its stil valid...");
+  const token = JSON.parse(Keychain.get("bmw_access_token"));
 
   if (Math.floor(Date.now() / 1000) >= token.expires_at) {
-    console.log('existing token expired...');
+    console.log("existing token expired...");
     if (!(await requestAPIToken())) return;
     return getAPIToken();
   }
 
-  console.log('valid token found!');
+  console.log("valid token found!");
   return token.access_token;
 }
 
 async function requestAPIToken() {
-  console.log('requesting API token...');
+  console.log("requesting API token...");
 
-  const URL = 'https://customer.bmwgroup.com/gcdm/oauth/token';
+  const URL = "https://customer.bmwgroup.com/gcdm/oauth/token";
   const OAuthClientData = {
     grant_type: "password",
     scope: "openid profile email offline_access smacc vehicle_data perseus dlm svds cesim vsapi remote_services fupo authenticate_user",
@@ -82,21 +82,23 @@ async function requestAPIToken() {
     password: configuration.PASSWORD,
   };
   const data = utils.encodeAsQueryString(OAuthClientData);
+  console.log("Query: " + data);
 
   let req = new Request(`${URL}`);
-  req.method = 'POST';
+  req.method = "POST";
   req.headers = {
     Authorization:
-      'Basic MzFjMzU3YTAtN2ExZC00NTkwLWFhOTktMzNiOTcyNDRkMDQ4OmMwZTMzOTNkLTcwYTItNGY2Zi05ZDNjLTg1MzBhZjY0ZDU1Mg==',
-    Credentials: 'nQv6CqtxJuXWP74xf3CJwUEP:1zDHx6un4cDjybLENN3kyfumX2kEYigWPcQpdvDRpIBk7rOJ',
+      "Basic MzFjMzU3YTAtN2ExZC00NTkwLWFhOTktMzNiOTcyNDRkMDQ4OmMwZTMzOTNkLTcwYTItNGY2Zi05ZDNjLTg1MzBhZjY0ZDU1Mg==",
+    Credentials:
+      "nQv6CqtxJuXWP74xf3CJwUEP:1zDHx6un4cDjybLENN3kyfumX2kEYigWPcQpdvDRpIBk7rOJ",
   };
   req.body = data;
 
   let resp = await req.loadJSON();
 
-  if (!resp['access_token']) return false;
-  if (!resp['expires_in']) return false;
-  if (!resp['refresh_token']) return false;
+  if (!resp["access_token"]) return false;
+  if (!resp["expires_in"]) return false;
+  if (!resp["refresh_token"]) return false;
 
   const token = {
     access_token: resp.access_token,
@@ -104,6 +106,6 @@ async function requestAPIToken() {
     expires_at: Math.floor(Date.now() / 1000) + parseInt(resp.expires_in),
   };
 
-  Keychain.set('bmw_access_token', JSON.stringify(token));
+  Keychain.set("bmw_access_token", JSON.stringify(token));
   return true;
 }
